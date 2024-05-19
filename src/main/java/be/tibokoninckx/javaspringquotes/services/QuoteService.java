02f638b6ca58b0028d609ceec402eda9cd21d84d
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 18:03:11 +0200

    create a service to get quotes

--- src/main/java/be/tibokoninckx/javaspringquotes/services/QuoteService.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/services/QuoteService.java
@@ -1,4 +1,21 @@
 package be.tibokoninckx.javaspringquotes.services;
 
+import be.tibokoninckx.javaspringquotes.model.Quote;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
 public class QuoteService {
+    public List<Quote> getQuotes() {
+        try {
+            InputStream quoteFile = new ClassPathResource("data.json").getInputStream();
+            ObjectMapper objectMapper = new ObjectMapper();
+            return Arrays.asList(objectMapper.readValue(quoteFile, Quote[].class));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load quotes frol JSON file", e);
+        }
+    }
 }
