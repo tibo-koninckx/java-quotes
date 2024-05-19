96b9176df3644768fb8c70d054bb47eddd40785a
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 17:32:59 +0200

    setup project

---
+++ src/main/java/be/tibokoninckx/javaspringquotes/JavaSpringQuotesApplication.java
@@ -0,0 +1,13 @@
+package be.tibokoninckx.javaspringquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringQuotesApplication.class, args);
+    }
+
+}
