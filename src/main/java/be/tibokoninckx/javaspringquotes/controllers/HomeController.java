fcf50ad4c8ab9d3ca317aed63f8476f06054e911
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 19:06:42 +0200

    take random quotes

--- src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
@@ -1,9 +1,13 @@
 package be.tibokoninckx.javaspringquotes.controllers;
 
+import be.tibokoninckx.javaspringquotes.model.Quote;
 import be.tibokoninckx.javaspringquotes.services.QuoteService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 @Controller
 public class HomeController {
@@ -12,9 +16,13 @@ public class HomeController {
     public HomeController(QuoteService quoteService) {
         this.quoteService = quoteService;
     }
+    private Random random = new Random();
+
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("quotes", quoteService.getQuotes());
+        ArrayList<Quote> quotes = new ArrayList<>(quoteService.getQuotes());
+        model.addAttribute("randomQuote", quotes.get(random.nextInt(quotes.size())));
         return "index";
     }
 }
