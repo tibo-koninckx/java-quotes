da40762027e510a1911ac7fa4aec0a525ea13daf
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 17:45:36 +0200

    add home to controller

--- src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
@@ -1,7 +1,12 @@
 package be.tibokoninckx.javaspringquotes.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
 }
