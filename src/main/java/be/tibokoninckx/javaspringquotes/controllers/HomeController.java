de57d973a91b186b4c36738d41095d8fda2a1001
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 18:18:46 +0200

    add service to controller

--- src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package be.tibokoninckx.javaspringquotes.controllers;
 
+import be.tibokoninckx.javaspringquotes.services.QuoteService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final QuoteService quoteService;
+
+    public HomeController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("quotes", quoteService.getQuotes());
         return "index";
     }
 }
