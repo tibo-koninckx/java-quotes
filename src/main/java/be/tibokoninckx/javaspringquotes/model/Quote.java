c536bca236019cc637812979d0d3bb44ecb3af5b
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 17:48:27 +0200

    add constructor and getters and setters

--- src/main/java/be/tibokoninckx/javaspringquotes/model/Quote.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/model/Quote.java
@@ -1,4 +1,37 @@
 package be.tibokoninckx.javaspringquotes.model;
 
 public class Quote {
+    private Integer id;
+    private String QuoteText;
+    private String description;
+
+    public Quote(Integer id, String quoteText, String description) {
+        this.id = id;
+        QuoteText = quoteText;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuoteText() {
+        return QuoteText;
+    }
+
+    public void setQuoteText(String quoteText) {
+        QuoteText = quoteText;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
