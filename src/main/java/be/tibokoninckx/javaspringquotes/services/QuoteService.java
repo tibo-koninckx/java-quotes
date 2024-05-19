0875bb51aef24e9347d47ed84497a14d3d0467df
tibo-koninckx <r0929614@student.thomasmore.be>
2024-05-19 18:06:30 +0200

    add service annotation

--- src/main/java/be/tibokoninckx/javaspringquotes/services/QuoteService.java
+++ src/main/java/be/tibokoninckx/javaspringquotes/services/QuoteService.java
@@ -3,11 +3,13 @@ package be.tibokoninckx.javaspringquotes.services;
 import be.tibokoninckx.javaspringquotes.model.Quote;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class QuoteService {
     public List<Quote> getQuotes() {
         try {
